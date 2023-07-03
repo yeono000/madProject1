d2eee7eb89e97c75b1ffd4320c399474a5f5292b
yeono000 <tata0430@hanyang.ac.kr>
2023-07-03 13:57:49 +0900

    contact

---
+++ app/src/main/java/com/yeono/madproject1/LoginActivity.kt
@@ -0,0 +1,53 @@
+package com.yeono.madproject1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if (currentUser == null){
+            val loginButton = findViewById<Button>(R.id.login_button)
+            loginButton.setOnClickListener{
+                auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        updateUI(null)
+                    }
+                }
+            }
+        }
+        else{
+            updateUI(currentUser)
+        }
+    }
+    fun updateUI(user : FirebaseUser?){
+        Log.d("user", user.toString())
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 0)
+    }
+}
